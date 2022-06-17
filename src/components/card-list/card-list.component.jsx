b0e14161f2e363ebf8dd59e529fb090901ef69ad
Steven Li <sxli@buffalo.edu>
2022-06-17 19:52:43 -0400

    Finished Monsters Rolodex, my first React project

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+import Card from '../card/card.component';
+import './card-list.styles.css';
+
+const CardList = ({monsters}) => {
+    return(
+    <div className = 'card-list'>           
+        {monsters.map((monster) => {
+            const { name, id, email } = monster;
+            return (
+                <Card monster = {monster}></Card>
+            )                  
+            })}    
+    </div>
+    );    
+};
+
+
+/*
+class CardList extends Component {
+    render() {
+        const { monsters } = this.props;
+        return (
+            <div className = 'card-list'>           
+                {monsters.map((monster) => {
+                    const { name, id, email } = monster;
+                    return (
+                        <Card monster = {monster}></Card>
+                    )                  
+                    })}    
+            </div>
+        );
+    }
+} */
+
+export default CardList;
